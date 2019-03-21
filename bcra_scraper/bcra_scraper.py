3be981bdf494db1aee69e59439ceeffcf077d89a
Alan Sosa <alan.sosa@datalytics.com>
2019-03-21 15:48:20 -0300

    Agrego comando con click y funcion main y run para scrapear datos del ultimo dia habil anterior

--- bcra_scraper/bcra_scraper.py
+++ bcra_scraper/bcra_scraper.py
@@ -1,9 +1,18 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+from datetime import date
 
+import click
+
+from bcra_scraper.scraper import Scraper
+
+
+@click.command()
 def main():
-    pass
+    scraper = Scraper()
+    parsed = scraper.run()
+    click.echo(parsed)
 
 if __name__ == '__main__':
     main()
---
+++ tests/test_bcra_scraper.py
@@ -0,0 +1,15 @@
+import unittest
+
+import click
+from click.testing import CliRunner
+
+from bcra_scraper.bcra_scraper import main
+
+
+class MainTest(unittest.TestCase):
+
+    def test_main(self):
+        runner = CliRunner()
+        result = runner.invoke(main)
+
+        assert result.exit_code == 0
