a9ec1914e9365aa89e78621080b133b16accf683
Alan Sosa <alan.sosa@datalytics.com>
2019-03-26 16:20:41 -0300

    Agrego funcionalidad para guardar contenido scrapeado dentro de archivo csv

--- bcra_scraper/bcra_scraper.py
+++ bcra_scraper/bcra_scraper.py
@@ -1,12 +1,19 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+from csv import DictWriter
 from datetime import date
 
 import click
 
 from bcra_scraper.scraper import Scraper
 
+# TODO: test me!
+def write_tasas_libor(file_name, header, rows):
+    with open(file_name, 'w') as archivo:
+        writer = DictWriter(archivo, fieldnames=header)
+        writer.writeheader()
+        writer.writerows(rows)
 
 def get_default_start_date():
     today = date.today()
@@ -18,22 +25,9 @@ def get_default_end_date():
 
     return f'{today.day}/{today.month}/{today.year}'
 
-# def arg1_validator(ctx, param, value):
-#     breakpoint()
-#     if ctx.args is not []:
-#         raise click.BadParameter('Should be a positive, even integer.')
-#     else:
-#         return value
 
-# def arg_validator(ctx, param, value):
-#     breakpoint()
-#     if ctx.args is not []:
-#         raise click.BadParameter('Should be a positive, even integer.')
-#     else:
-#         return value
-
-# validar q no se ingrese solo end_date
-# validar q end_date >= start_date
+# TODO: validar q no se ingrese solo end_date
+# TODO: validar q end_date >= start_date
 @click.command()
 @click.option(
     '--start-date',
@@ -51,5 +45,13 @@ def main(start_date, end_date):
     start_date = date(start_date.year, start_date.month, start_date.day)
     end_date = date(end_date.year, end_date.month, end_date.day) 
     scraper = Scraper()
+
     parsed = scraper.run(start_date, end_date)
-    click.echo(parsed)
+
+    if parsed:
+        csv_name = 'tasas-libor.csv'
+        csv_header = ['indice_tiempo', '30', '60', '90', '180', '360']
+
+        write_tasas_libor(csv_name, csv_header, parsed)
+    else:
+        click.echo("No se encontraron resultados")
