1747fa3a780680ad45e240d24025102865528463
Alan Sosa <alan.sosa@datalytics.com>
2019-04-08 17:49:22 -0300

    Se agregan test para las funciones de ExchangeRate

---
+++ tests/test_bcra_exchange_rate.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Tests del modulo bcrascraper."""
+
+from __future__ import unicode_literals
+from __future__ import print_function
+from __future__ import with_statement
+
+import datetime
+from datetime import date, timedelta
+import unittest
+
+from bcra_scraper.scraper import ExchangeRateScraper
+
+class BcraExchangeRateTestCase(unittest.TestCase):
+
+    def test_parse_for_empty_contents(self):
+        scraper = ExchangeRateScraper()
+        end_date = date.today()
+        contents = {}
+        parsed = scraper.parse(contents, end_date)
+
+        assert parsed == []
+
+    def test_parse_for_non_empty_contents(self):
+        scraper = ExchangeRateScraper()
+        end_date = datetime.datetime(2019, 4, 8)
+        contents = {}
+        
+        table_content = '''
+        <table class="table table-BCRA table-bordered table-hover table-responsive" colspan="3">
+            <thead>
+            <tr>
+            <td colspan="3">
+                <b>MERCADO DE CAMBIOS - COTIZACIONES CIERRE VENDEDOR<br>Bolívar Venezolano</b>
+            </td>
+            </tr>
+            <tr>
+                <td width="10%"><b>
+                    FECHA</b>
+                </td>
+                <td width="40%"><b>
+            TIPO DE PASE - EN DOLARES - (por unidad)</b></td>
+                <td width="50%"><b>
+            TIPO DE CAMBIO - MONEDA DE CURSO LEGAL - (por unidad)</b></td>
+                </tr>
+            </thead>
+            <tbody><tr>
+                <td width="10%">
+                08/04/2019</td>
+                <td width="40%">
+                0,0003030</td>
+                <td width="50%">
+                0,0132500</td>
+            </tr>
+            </tbody>
+        </table>
+        '''
+        
+        contents['bolivar_venezolano'] = table_content
+
+        parsed = scraper.parse(contents, end_date)
+
+        assert parsed == [{'moneda': 'bolivar_venezolano',
+        'indice_tiempo': '08/04/2019', 'tipo_pase': '0,0003030',
+        'tipo_cambio': '0,0132500'}]
+
+    def test_parse_coin(self):
+        scraper = ExchangeRateScraper()
+        end_date = datetime.datetime(2019, 4, 8)
+        coin = 'bolivar_venezolano'
+        
+        content = '''
+        <table class="table table-BCRA table-bordered table-hover table-responsive" colspan="3">
+            <thead>
+            <tr>
+            <td colspan="3">
+                <b>MERCADO DE CAMBIOS - COTIZACIONES CIERRE VENDEDOR<br>Bolívar Venezolano</b>
+            </td>
+            </tr>
+            <tr>
+                <td width="10%"><b>
+                    FECHA</b>
+                </td>
+                <td width="40%"><b>
+            TIPO DE PASE - EN DOLARES - (por unidad)</b></td>
+                <td width="50%"><b>
+            TIPO DE CAMBIO - MONEDA DE CURSO LEGAL - (por unidad)</b></td>
+                </tr>
+            </thead>
+            <tbody><tr>
+                <td width="10%">
+                08/04/2019</td>
+                <td width="40%">
+                0,0003030</td>
+                <td width="50%">
+                0,0132500</td>
+            </tr>
+            </tbody>
+        </table>
+        '''
+
+        parsed_coin = scraper.parse_coin(content, end_date, coin)
+
+        assert parsed_coin == [{'moneda': 'bolivar_venezolano',
+        'indice_tiempo': '08/04/2019', 'tipo_pase': '0,0003030',
+        'tipo_cambio': '0,0132500'}]
