87ea9c23a9ecc47422fc7dc6b5ecb1d2318ba9a0
Marcos Damián Mesmer y Rosset <marcosdmyr@bitson.group>
2019-09-25 12:34:17 -0300

    Revert "Merge branch 'bcra-reverse-days' into 'master'"
    This reverts merge request !18

--- bcra_scraper/scraper_tce.py
+++ bcra_scraper/scraper_tce.py
@@ -222,7 +222,6 @@ class BCRATCEScraper(BCRAScraper):
         intermediate_panel_data.extend(
             parsed_contents['dolar'] + parsed_contents['euro']
         )
-        intermediate_panel_data.reverse()
         return intermediate_panel_data
 
     def parse_from_intermediate_panel(self, start_date, end_date):
@@ -305,8 +304,6 @@ class BCRATCEScraper(BCRAScraper):
 
                         if parsed_row:
                             parsed[coin].append(parsed_row)
-        parsed['dolar'].reverse()
-        parsed['euro'].reverse()
         return parsed
 
     def write_intermediate_panel(self, rows, intermediate_panel_path):
@@ -340,7 +337,7 @@ class BCRATCEScraper(BCRAScraper):
 
         try:
             intermediate_panel_dataframe = pd.read_csv(
-                'tce-intermediate-panel.csv',
+                '.tce-intermediate-panel.csv',
                 converters={
                     'serie_tiempo': lambda _: _,
                     'coin': lambda _: str(_),
